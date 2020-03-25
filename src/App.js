14c25a438bf74a8dd76238f86cc45cd1b475579f
JEAN PAULO DE OLIVEIRA ALVES <jean.alves@indigosoft.com.br>
2020-03-25 18:11:53 -0300

    projeto em react omnistack

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Header from './Header'
+import './global.css';
+import Routes from './routes';
+
+function App() {
+  return (
+    // <Header title="Semana omnistack" />
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+
+export default App;
